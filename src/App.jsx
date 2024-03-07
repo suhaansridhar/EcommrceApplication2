765bed8bc6f78e485d5498d17d9e54a79f924ec3
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2024-03-07 18:47:59 +0000

    added prices and ratings to the objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import React from 'react/index.js';
+
 import SideBar from './Components/SideBar';
 import MainContainer from './Components/MainContainer';
-import { createContext } from 'react/index.js';
 
 import photo1 from './assets/LouisVuitton_00.webp';
 import photo2 from './assets/LouisVuitton_01.webp';
@@ -18,32 +19,29 @@ import { useEffect } from 'react/index.js';
 
 
 function App(){
-  const [array, setArray] = useState(
-    {title: "Louis Vuitton", source: photo1},
-    {title: "Louis Vuitton", source: photo2},
-    {title: "Louis Vuitton", source: photo3},
-    {title: "Louis Vuitton", source: photo4},
-    {title: "Louis Vuitton", source: photo5},
-    {title: "Louis Vuitton", source: photo11},
-    {title: "Saint Laurent", source: photo6},
-    {title: "Saint Laurent", source: photo7},
-    {title: "Saint Laurent", source: photo8},
-    {title: "Saint Laurent", source: photo9},
-    {title: "Saint Laurent", source: photo10}
-    );
+  const [array, setArray] = useState([
+    {title: "Louis Vuitton", source: photo1, price: 389, rating: "4.2/5"},
+    {title: "Louis Vuitton", source: photo2, price: 990, rating: "4.0/5"},
+    {title: "Louis Vuitton", source: photo3, price: 512, rating: "3.9/5"},
+    {title: "Louis Vuitton", source: photo4, price: 614, rating: "4.4/5"},
+    {title: "Louis Vuitton", source: photo5, price: 800, rating: "4.9/5"},
+    {title: "Louis Vuitton", source: photo11, price: 899, rating: "4.3/5"},
+    {title: "Saint Laurent", source: photo6, price: 987, rating: "4.4/5"},
+    {title: "Saint Laurent", source: photo7, price: 999, rating: "3.9/5"},
+    {title: "Saint Laurent", source: photo8, price: 399, rating: "4.3/5"},
+    {title: "Saint Laurent", source: photo9, price: 499, rating: "4.8/5"},
+    {title: "Saint Laurent", source: photo10, price: 799, rating: "4.5/5"}
+  ]);
 
   const [duplicate, setDuplicate] = useState();
-
-
 
   useEffect(() => {
     setDuplicate(array);
   }, []);
 
-
   return(
       <div className="app--container">
-        <h1>Ecommerce Website</h1>
+        <h1>Ecommerce Website <span>prices in euros</span></h1>
         <div className="app--inner--container">
           <SideBar />
           <MainContainer />
