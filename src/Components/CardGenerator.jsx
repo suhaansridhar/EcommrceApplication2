3ddd825b1285b7adaffe4584b2285a66a92d25dd
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2024-03-09 11:33:49 +0000

    updated cards component

--- src/Components/CardGenerator.jsx
+++ src/Components/CardGenerator.jsx
@@ -1,7 +1,19 @@
-function CardGenerator() {
+function CardGenerator(props) {
+  // const array = props.array;
+  console.log(props.array);
+
   return (
     <div className="card--generator--container">
-        
+        {props.array.map((element, index) => {
+          return(
+            <div className = "card--container" key={index}>
+              <img src={props.array.source} />
+              <h4>{props.array.title}</h4>
+              <h4>{props.array.price}</h4>
+              <h4>{props.array.rating}</h4>
+            </div>
+          )
+        })}
     </div>
   )
 }
